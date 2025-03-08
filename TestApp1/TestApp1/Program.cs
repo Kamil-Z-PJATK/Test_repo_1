384b9b72d3c6a8aa47530ad0bf9f6be5d275d332
s30756 <s30756@pjwstk.edu.pl>
2025-03-08 19:55:26 +0100

    Added avg function

--- TestApp1/TestApp1/Program.cs
+++ TestApp1/TestApp1/Program.cs
@@ -5,3 +5,21 @@ int x=15;
 int y = 44;
 Console.WriteLine(x);
 Console.WriteLine(x+y);
+Main([]);
+ static void Main(string[] args)
+{
+    
+    List<int> l1 = new List<int>{1,2,3,4,5,6,7,8,9,10};
+   Console.WriteLine(avg(l1)) ;
+
+}
+
+static double avg(List<int> l1)
+{
+    double sum = 0;
+    for (int i = 0; i < l1.Count; i++)
+    {
+        sum += l1[i];
+    }
+    return sum/l1.Count;
+}
